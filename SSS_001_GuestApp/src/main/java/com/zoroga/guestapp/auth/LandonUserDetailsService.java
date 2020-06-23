56792c652e50126f2d5ca6e624acb860cedf9dd7
Zoltan Gabor <Zoltan_Gabor@epam.com>
2020-06-23 14:52:02 +0200

    Add basic, in-memory and JDBC authentication

---
+++ SSS_001_GuestApp/src/main/java/com/zoroga/guestapp/auth/LandonUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.zoroga.guestapp.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LandonUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (null == user) {
+			throw new UsernameNotFoundException("Cannot find username: " + username);
+		}
+
+		return new LandonUserPrincipal(user);
+	}
+}
